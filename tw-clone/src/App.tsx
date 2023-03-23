346ac13ebf667962b27e5e740aa4a3609ea735be
Kaito <haidoushi875@gmail.com>
2023-03-23 14:20:11 +0900

    ルーティング設定

--- tw-clone/src/App.tsx
+++ tw-clone/src/App.tsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthLayout } from "./components/layout/AuthLayout";
+import { Login } from "./components/Pages/Login";
+import { Register } from "./components/Pages/Register";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+
 const App = () => {
-  return <div>Hello React</div>;
+  const theme = createTheme({ palette: { primary: { main: "#0000ff" } } });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
 
 export default App;
